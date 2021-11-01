f570845266649c1d44b4d2e363b10994c0aae507
Anna Emson <17829828+AnnaEmson@users.noreply.github.com>
2021-11-01 21:22:05 +0000

    add public endpoint fetch

--- src/Private.js
+++ src/Private.js
@@ -1,6 +1,27 @@
-const Private = () => {
+import React, {useState, useEffect} from "react";
+
+const Private = (props) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/private", {
+      headers: {
+        Authorization: `Bearer ${props.auth.getAccessToken()}`
+      }
+    })
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error("Network response was not ok.");
+    })
+    .then(response => setMessage(response.message))
+    .catch(error => setMessage(error.message))
+  }, [])
   return (
-  <h1>Private</h1> );
+    <>
+      <h1>Private</h1>
+      {message}
+    </>
+  );
 }
 
 export default Private;
